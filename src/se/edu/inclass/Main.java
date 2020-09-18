f9ff1d98193533b1d985ad175114f5a8ae08d457
Ang Cheng Jun <e0415830@u.nus..edu>
2020-09-18 17:24:36 +0800

    Main function modify

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,12 +16,15 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        printData(tasksData);
+        //printData(tasksData);
         System.out.println("Printing deadlines");
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        //printDataUsingStreams(tasksData);
+        printDeadlineUsingStreams(tasksData);
+        System.out.println("Total number of deadlines: " + countDeadlinesUsingStreams(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
